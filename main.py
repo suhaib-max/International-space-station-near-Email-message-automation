f3309b8b2e956522565b54a7a09f19317dc1a2a7
suhaib <suhaibvbm@gmail.com>
2023-11-14 20:31:40 +0530

    checking internation space station is closed to my long, lati

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ MY_LONG = 76.044945  # Your longitude
 #Give email and password
 EMAIL = "sshuhaib274@gmail.com"
 PASSWORD = "App_password"
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude >= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True

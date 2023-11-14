57d871471ad70f9e44a7958b391f448a4b801a34
suhaib <suhaibvbm@gmail.com>
2023-11-14 20:22:14 +0530

    requirements added

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+# Find you locational longitude and latitude
+MY_LAT = 11.127693  # Your latitude
+MY_LONG = 76.044945  # Your longitude
+
+#Give email and password
+EMAIL = "sshuhaib274@gmail.com"
+PASSWORD = "App_password"

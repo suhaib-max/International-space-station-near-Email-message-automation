5dcfc3bc8f7eb6094155efdf4b91205a813c26d8
suhaib <suhaibvbm@gmail.com>
2023-11-14 20:41:00 +0530

    function: is want in night conditioning using (sunset sunrise api)

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def is_iss_overhead():
     # Your position is within +5 or -5 degrees of the ISS position.
     if MY_LAT - 5 <= iss_latitude >= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
         return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    print(time_now)
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
